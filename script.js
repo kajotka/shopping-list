0ef77bfd8e9791544bce8ac8ccabef82db746f36
kajotka <karolina.janicka@edu.uekat.pl>
2023-03-09 11:05:01 +0100

    readme + backend

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 var sumy = {
-    Nalogi: 0,
-    Kosmetyki: 0,
-    Jedzenie: 0,
-    Rachunki: 0,
-    Komunikacja: 0,
-    Zdrowie: 0,
-    Inne: 0,
-    Wszystkie: 0
+  Nalogi: 0,
+  Kosmetyki: 0,
+  Jedzenie: 0,
+  Rachunki: 0,
+  Komunikacja: 0,
+  Zdrowie: 0,
+  Inne: 0,
+  Wszystkie: 0,
 };
 
+// backend
 var api_host = 'http://localhost:5001';
 
 function dodaj() {
-    let kategoria = document.querySelector("select").value;
-    if (kategoria === "Wybierz kategorię") {
-        return;
-    }
+  let kategoria = document.querySelector("select").value;
+  if (kategoria === "Wybierz kategorię") {
+    return;
+  }
 
-    let produkt = document.getElementById("pole1").value;
-    let cena = document.getElementById("pole2").value;
-    let table = document.getElementById("tablica");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = kategoria;
-    cell2.innerHTML = produkt;
-    cell3.innerHTML = cena;
-    cell3.className = "pln";
-    dodajDoSumy(kategoria, cena);
+  let produkt = document.getElementById("pole1").value;
+  let cena = document.getElementById("pole2").value;
+  let table = document.getElementById("tablica");
+  var row = table.insertRow(-1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = kategoria;
+  cell2.innerHTML = produkt;
+  cell3.innerHTML = cena;
+  cell3.className = "pln";
+  dodajDoSumy(kategoria, cena);
 
+
+//backend
     fetch('http://localhost:5001/zakup/add', {
         method: 'POST',
         headers: {
@@ -44,34 +47,26 @@ function dodaj() {
     }).then(function (response) {
     });
     render();
-}
 
+}
 
 let suma = 0;
 const pln = document.querySelectorAll(".pln");
 pln.forEach(function (pojedynczyPln) {
-    suma += parseInt(pojedynczyPln.innerHTML);
-})
-
+  suma += parseInt(pojedynczyPln.innerHTML);
+});
 
 let sumaWszystkiego = document.querySelector("#sumaWszystkiego");
 sumaWszystkiego.innerHTML = suma;
 
 function dodajDoSumy(kategoria, cena) {
-    sumy[kategoria] += Number(cena);
-    document.querySelector("#suma"+ kategoria).innerHTML = sumy[kategoria];
-    sumy.Wszystkie += Number(cena);
-    document.querySelector("#sumaWszystkiego").innerHTML =  sumy.Wszystkie;
- 
-    // if (kategoria === "Jedzenie") {
-    //     sumaJedzenie += Number(cena);
-    //     suma += Number(cena);
-    //     document.querySelector("#sumaJedzenie").innerHTML = sumaJedzenie;
-    //     document.querySelector("#sumaWszystkiego").innerHTML = suma;
-    // }
-   
+  sumy[kategoria] += Number(cena);
+  document.querySelector("#suma" + kategoria).innerHTML = sumy[kategoria];
+  sumy.Wszystkie += Number(cena);
+  document.querySelector("#sumaWszystkiego").innerHTML = sumy.Wszystkie;
 }
 
+//backend
 function wczytaj() {
     fetch('http://localhost:5001/zakup/get').then(function (response) {
         response.json().then(function (json) {
@@ -93,39 +88,45 @@ function wczytaj() {
 
 wczytaj();
 
-
-$('#kategoria').select2({
-    placeholder: 'Select an option'
+$("#kategoria").select2({
+  placeholder: "Select an option",
 });
 
-
-
 function render() {
-    const ctx = document.getElementById('myChart2');
-    if(window.chart!= null)
-    {
-     window.chart.destroy();
-    }
-    window.chart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: ['Kosmetyki', 'Jedzenie', 'Nałogi', "Zdrowie", "Komunikacja", "Rachunki", "Inne"],
-            datasets: [{
-                label: 'Wykres wydatkow',
-                data: [sumy.Kosmetyki, sumy.Jedzenie, sumy.Nalogi, sumy.Zdrowie, sumy.Komunikacja, sumy.Rachunki, sumy.Inne],
-                borderWidth: 1
-            }]
+  const ctx = document.getElementById("myChart2");
+  if (window.chart != null) {
+    window.chart.destroy();
+  }
+  window.chart = new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      labels: [
+        "Kosmetyki",
+        "Jedzenie",
+        "Nałogi",
+        "Zdrowie",
+        "Komunikacja",
+        "Rachunki",
+        "Inne",
+      ],
+      datasets: [
+        {
+          label: "Wykres wydatkow",
+          data: [
+            sumy.Kosmetyki,
+            sumy.Jedzenie,
+            sumy.Nalogi,
+            sumy.Zdrowie,
+            sumy.Komunikacja,
+            sumy.Rachunki,
+            sumy.Inne,
+          ],
+          borderWidth: 1,
         },
-        options: {
-            
-        }
-    });
+      ],
+    },
+    options: {},
+  });
 }
 
-
 render();
-
-// $(document).ready( function () {
-//     $('#tablica').DataTable();
-    
-// } );
